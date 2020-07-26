76bc4706e2594288a76982d1bf1424a4152d14c7
sharminex81 <mimetyshanta@gmail.com>
2020-07-26 18:38:04 +0600

    DefaultController has been added.

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
+class DefaultController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class RoleController extends Controller
      */
     public function index()
     {
-        return view('dashboard.roles');
+
     }
 
     /**
