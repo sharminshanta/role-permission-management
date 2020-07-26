baf6f3e5e1026d266bf6941bd46ef365bfe7e2c5
sharminex81 <mimetyshanta@gmail.com>
2020-07-26 19:20:55 +0600

    Main route has been changed.

--- routes/web.php
+++ routes/web.php
@@ -11,32 +11,6 @@
 |
 */
 
-/**
- * Default page of this application
- * named dashboard route
- */
-Route::get('/', 'DashboardController@index');
-
-/**
- * Roles route
- * from where add, view, update and delete action can be done
- */
-Route::group(['prefix' => 'roles', /*'middleware' => ['auth.admin']*/], function () {
-    Route::get('/', 'RoleController@index'); //Role's list page
-});
-
-/**
- * Permissions route
- * from where add, view, update and delete action can be done
- */
-Route::group(['prefix' => 'permissions', 'middleware' => ['auth.admin']], function () {
-    Route::get('/', 'PermissionController@index'); //Permission's list page
-});
-
-/**
- * Settings route
- * from where add, view, update and delete action can be done
- */
-Route::group(['prefix' => 'settings', 'middleware' => ['auth.admin']], function () {
-    Route::get('/', 'SettingController@index'); //Setting page
-});
+Route::get('{any}', function () {
+    return view('layouts.app');
+})->where('any', '.*');
