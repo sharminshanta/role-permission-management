1ec3fd04d1aa26abef2635ab8dd410908b7aba01
sharminex81 <mimetyshanta@gmail.com>
2020-08-09 16:45:39 +0600

    Login is separated from others.

---
+++ resources/js/components/LoginTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <section class="content">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-4">
+                    <form method="post" enctype="multipart/form-data" style="margin-top: 40px">
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <label>Email Address</label>
+                                    <input type="email"  class="form-control" name="email_address" placeholder="Enter Email Address">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <label>Password</label>
+                                    <input type="password"  class="form-control" name="password" placeholder="Enter Password">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
