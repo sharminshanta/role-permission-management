5a251d6ee9738f823c239f853306c89a3a44d029
sharminex81 <mimetyshanta@gmail.com>
2020-08-09 16:43:46 +0600

    Route is refactored.

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,12 @@ Route::middleware('auth:api')->get('user', function (Request $request) {
 /*/**
  * Landing page for dashboard
  */
-Route::get('/', 'DefaultController@index'); //Landing Page of App
-Route::get('/dashboard', 'DashboardController@index'); //Landing Page of Dashboard
+//Route::get('/', 'DefaultController@index'); //Landing Page of App
+//Route::get('/dashboard', 'DashboardController@index'); //Landing Page of Dashboard
+
+/**
+ * Roles Routes
+ * All user defined role can be create, read, update and delete here
+ */
 Route::get('/roles', 'RoleController@getRole'); //Role view
 
